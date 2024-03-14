8af1323395c9a79428445652642525fa7bbcdb5b
simoneaucello <simoneaucello@gmail.com>
2024-03-14 13:03:20 +0100

    [add] v-for to contacts

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+import {contacts} from './users.js'
+
+const { createApp } = Vue;
+
+createApp({
+
+  data(){
+    return{
+      contacts,
+    }
+  },
+
+  methods: {
+
+  },
+
+
+
+  mounted(){
+
+  }
+
+}).mount('#app')
