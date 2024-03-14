d26483960cf06cb6a546cfddd14d7529c0b90dfe
simoneaucello <simoneaucello@gmail.com>
2024-03-14 15:57:41 +0100

    [add] milestone 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,21 @@ createApp({
   data(){
     return{
       contacts,
+      activeChat: 0
     }
   },
 
   methods: {
+
+    getLastMessage(contact) {
+      const lastMessage = contact.messages[contact.messages.length - 1];
+      return lastMessage ? lastMessage.message : '';
+    },
+
+    getLastDate(contact){
+      const lastDate = contact.messages[contact.messages.length - 1];
+      return lastDate ? lastDate.date : '';
+    }
 
   },
 
